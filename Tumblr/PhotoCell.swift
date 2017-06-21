9ced8cb5dc504e99e46c8f2c747a6c5a00f4962e
Chase Warren <chasejjw@fb.com>
2017-06-21 13:47:12 -0700

    Initial Commit

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Chase Warren on 6/21/17.
+//  Copyright © 2017 Chase Warren. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var PhotoCell: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
